c066ffaa4f6cf7a11d71d08d995dbcbfd7891b4c
fanghr <chfanghr@gmail.com>
2020-09-03 14:55:50 +0800

    fix: "/app/service/user.go:13 unsupported data type: &[]"

--- service/models/profile.go
+++ service/models/profile.go
@@ -1,6 +1,10 @@
 package models
 
+import "gorm.io/gorm"
+
 type Profile struct {
+	gorm.Model
+
 	Alias    string        `json:"alias"`
 	Matches  []MatchResult `json:"matches"`
 	Picture  []byte        `json:"picture"`
